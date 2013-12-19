b4be4824f1e853de230bb2ca0fb8b0b6918d9ba6
SirJulio <sirjuliotrashbox@gmail.com>
2013-12-20 00:09:39 +0100

    Version number and license.

--- src/LightweightUINew.cs
+++ src/LightweightUINew.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿//Copyright © 2013 Dagorn Julien (julien.dagorn@gmail.com)
+//This work is free. You can redistribute it and/or modify it under the
+//terms of the Do What The Fuck You Want To Public License, Version 2,
+//as published by Sam Hocevar. See the COPYING file for more details.
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
